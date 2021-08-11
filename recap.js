926789f9fdb9302fbdc8f1c6846e14bc219b05aa
sharifhassan05 <sharifhassan05@gmail.com>
2021-08-11 16:58:53 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,30 @@
+// variable
+
+var time = '10:55pm';
+var bookPrice = 150;
+var isSkyGreen = false;
+
+//array
+
+var num = [2, 3 ,55, 98, 76, 19];
+console.log (num);
+
+var elementCount = num.length;
+console.log(elementCount);
+
+var ninetyeightIndex = num.indexOf(98);
+console.log(ninetyeightIndex);
+
+num.push (999);
+console.log(num);
+
+num.pop (3);
+console.log(num);
+
+
+//conditionals
+
+if(bookPrice<120){
+    console.log('I will buy this book');
+}
+
